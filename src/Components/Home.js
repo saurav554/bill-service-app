27715d7b3827a286fab15776848bf98c2336d482
saurav554 <67870601+saurav554@users.noreply.github.com>
2021-04-16 10:55:58 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import ProductsManagement from './ProductsManagement';
 
-class Home extends Component {
-  render(){
+function Home () {
+  
         return(
             <ProductsManagement />
         )
     }
-}
+
 
 export default Home;
